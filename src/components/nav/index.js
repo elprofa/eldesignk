6c2a573cc237c4a7d2cf8d7efcd6128f5113f073
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-04-30 11:14:42 +0000

    Push-02

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,10 +1,5 @@
 import * as React from "react"
 import { useState } from 'react';
-//import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-//import Nav from 'react-bootstrap/Nav'
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
     Collapse,
@@ -22,9 +17,8 @@ import {
   } from 'reactstrap';
 import NavBarStc from './nav.stc';
 
-import { button, nav} from "bootstrap"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+//import PropTypes from "prop-types"
+//import { Link } from "gatsby"
 
 const NavBar = (props) => {
       const [isOpen, setIsOpen] = useState(false);
@@ -35,34 +29,9 @@ const NavBar = (props) => {
         <div>
         <NavBarStc>
           <Navbar className='navbar' light expand="md">
-            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarBrand href="/">DesignKoncept</NavbarBrand>
             <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-              <Nav className="mr-auto" navbar>
-                <NavItem>
-                  <NavLink href="/components/">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Options
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>
-                      Option 1
-                    </DropdownItem>
-                    <DropdownItem>
-                      Option 2
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                      Reset
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
+            <Collapse className='my-3' isOpen={isOpen} navbar>
               <NavbarText className='mx-1'>Acceuil</NavbarText><br/>
               <NavbarText className='mx-1'>Catalogue</NavbarText><br/>
               <NavbarText className='mx-1'>Contact</NavbarText>
